e67601bcc74207885dee2577a3ce637b78417e26
Maax <maaxcheban@gmail.com>
2018-03-12 23:51:50 +0200

    without tests

---
+++ src/com/Graph/Edge.java
@@ -0,0 +1,39 @@
+package com.Graph;
+
+/**
+ * Created by MAX on 12.03.2018.
+ */
+public class Edge<V> {
+    private Vertex<V> source;
+    private Vertex<V> destination;
+    private int weight;
+
+    public Edge(Vertex<V> source, Vertex<V> destination, int weight) {
+        super();
+        this.source = source;
+        this.destination = destination;
+        this.weight = weight;
+    }
+
+    public Vertex<V> getSource() {
+        return source;
+    }
+
+    public Vertex<V> getDestination() {
+        return destination;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public boolean contain(Vertex<V> vertex){
+        return vertex.equals(source) || vertex.equals(destination);
+    }
+
+    @Override
+    public String toString() {
+        return "Edge [source=" + source + ", destination=" + destination
+                + ", weight=" + weight + "]";
+    }
+}
---
+++ src/com/Graph/Graph.java
@@ -0,0 +1,72 @@
+package com.Graph;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Created by MAX on 12.03.2018.
+ */
+public class Graph<V> {
+    private List<Vertex<V>> vertexes;
+    private List<Edge<V>> edges;
+    private Scanner scanner;
+
+    public int getVerticesCount() {
+        return vertexes.size();
+    }
+
+    public int getEdgeCount() {
+        return edges.size();
+    }
+
+    public Graph() {
+        super();
+        this.vertexes = new ArrayList<>();
+        this.edges = new ArrayList<>();
+        scanner = new Scanner(System.in);
+    }
+
+    public List<Vertex<V>> getVertexes() {
+        return vertexes;
+    }
+
+    public List<Edge<V>> getEdges() {
+        return edges;
+    }
+
+    public void addVertex(Vertex<V> vertex){
+        vertexes.add(vertex);
+    }
+
+    public void removeVertex(Vertex<V> vertex) {
+        for (Edge<V> edge: edges) {
+            if (edge.contain(vertex)){
+                edges.remove(edge);
+            }
+        }
+        vertexes.remove(vertex);
+    }
+
+    public void addEdge(Vertex<V> source, Vertex<V> dest, int weight){
+        Edge<V> edge = new Edge<>(source, dest, weight);
+        edges.add(edge);
+    }
+
+    public void removeEdge(Edge<V> edge){
+        edges.remove(edge);
+    }
+
+    public List<Vertex<V>> findShortestWay(Algorithm algorithm){
+        System.out.println("Print start position");
+        String startPos = scanner.nextLine();
+
+        System.out.println("Print destination");
+        String dest = scanner.nextLine();
+
+        algorithm.execute(new Vertex(startPos));
+        return algorithm.getPath(new Vertex(dest));
+    }
+
+
+}
